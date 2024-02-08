9fbfcc91d6719d2325044b887a1067d2a58c483e
kaichungss <116082052+kaichungss@users.noreply.github.com>
2024-02-08 12:30:43 +0000

    index

--- index.ts
+++ index.ts
@@ -2,54 +2,54 @@ import Koa from "koa";
 import Router, { RouterContext } from "koa-router";
 import logger from "koa-logger";
 import json from "koa-json";
-import bodyParser from "koa-bodyparser";
-import { CustomErrorMessageFunction, query, body, validationResults } from "koa-req-validation";
+//import bodyParser from "koa-bodyparser";
+//import { CustomErrorMessageFunction, query, body, validationResults } from "koa-req-validation";
 
 const app: Koa = new Koa();
-const router: Router = new Router();
+// const router: Router = new Router();
 
-const customErrorMessage: CustomErrorMessageFunction = (
-  _ctx: RouterContext,
-  value: string
-) => {
-  return (
-    `The name has to be between 3 and 20 ` +
-    `characters long but received length ${value.length}`
-  );
-};
+// const customErrorMessage: CustomErrorMessageFunction = (
+//   _ctx: RouterContext,
+//   value: string
+// ) => {
+//   return (
+//     `The name has to be between 3 and 20 ` +
+//     `characters long but received length ${value.length}`
+//   );
+// };
 
-const validatorName = [
-  body("name").isLength({ min: 3 }).withMessage(customErrorMessage).build(),
-  body("id").isInt({ min: 10000, max: 20000 }).build()
-];
+// const validatorName = [
+//   body("name").isLength({ min: 3 }).withMessage(customErrorMessage).build(),
+//   body("id").isInt({ min: 10000, max: 20000 }).build()
+// ];
 
-router.get('/',
-  query("name")
-    .isLength({ min: 3 }).optional()
-    .withMessage(customErrorMessage)
-    .build(),
-  async (ctx: RouterContext, next: any) => {
-    const result = validationResults(ctx);
-    if (result.hasErrors()) {
-      ctx.status = 422;
-      ctx.body = { err: result.mapped() }
-    } else {
-      ctx.body = { msg: `Hello world! ${ctx.query.name}` };
-    }
-    await next();
-  });
+// router.get('/',
+//   query("name")
+//     .isLength({ min: 3 }).optional()
+//     .withMessage(customErrorMessage)
+//     .build(),
+//   async (ctx: RouterContext, next: any) => {
+//     const result = validationResults(ctx);
+//     if (result.hasErrors()) {
+//       ctx.status = 422;
+//       ctx.body = { err: result.mapped() }
+//     } else {
+//       ctx.body = { msg: `Hello world! ${ctx.query.name}` };
+//     }
+//     await next();
+//   });
 
-router.post('/', ...validatorName, async (ctx: RouterContext, next: any) => {
-  const result = validationResults(ctx)
-  if (result.hasErrors()) {
-    ctx.status = 422;
-    ctx.body = { err: result.mapped() }
-  } else {
-    const data: any = ctx.request.body;
-    ctx.body = { respname: data.name };
-  }
-  await next();
-});
+// router.post('/', ...validatorName, async (ctx: RouterContext, next: any) => {
+//   const result = validationResults(ctx)
+//   if (result.hasErrors()) {
+//     ctx.status = 422;
+//     ctx.body = { err: result.mapped() }
+//   } else {
+//     const data: any = ctx.request.body;
+//     ctx.body = { respname: data.name };
+//   }
+//   await next();
+// });
 
 
 app.use(json());
